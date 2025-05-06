c8bbd835f19653b70ec5071c60423f118756c97c
oridotart <pstieg@wisc.edu>
2025-05-05 22:40:39 -0500

    Delete deepseek_javascript_20250506_177499.js

--- deepseek_javascript_20250506_177499.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const image = document.getElementById('cover-image');
-    if (Math.random() > 0.5) {
-        image.classList.add('inverted');
-        document.body.style.backgroundColor = 'black';
-    }
-});
